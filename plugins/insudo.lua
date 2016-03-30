ea03b167f76d477a63bc2c37835c6c60a81fc692
telegrambot81 <telegrambot81@gmail.com>
2016-03-30 11:42:34 +0430

    Update insudo.lua

--- plugins/insudo.lua
+++ plugins/insudo.lua
@@ -1,6 +1,7 @@
 do
 local function run(msg, matches)
-local sudo = 122774063 
+local sudo = 106789762
+ 
     if matches[1] == "insudo" and is_owner(msg) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
     end
